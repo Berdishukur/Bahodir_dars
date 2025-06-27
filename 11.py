6fb8824df8ddcdb0723358b003889313c9ea2e75
Berdishukur <boboqulovberdishukur@gmail.com>
2025-06-27 15:51:50 +0500

    add

--- 11.py
+++ 11.py
@@ -1,9 +1,3 @@
-import enum
-from enum import Enum
-class Color(Enum):
-    red = 1
-    green = 2
-    blue = 3
-print(Color.red) # Color.red
-print(Color(1)) # Color.red
-print(Color['red']) # Color.red
+
+matn=input("Matnni kiriting : ")
+print(matn.islower())
