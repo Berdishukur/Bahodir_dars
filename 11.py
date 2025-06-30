782cca369a3df1a7481db103b6ee762baabe8b7c
Berdishukur <boboqulovberdishukur@gmail.com>
2025-06-30 17:27:27 +0500

    add

--- 11.py
+++ 11.py
@@ -1,6 +1,10 @@
 # ism="Ali"
 # familiya="Valiyev"
 # yosh=22
-bozorliklar=["Olma","Tuz","Shakar","Kartoshka","Olma","Anor"]
-bozorliklar.reverse()
-print(bozorliklar)
+nums=[2,4,12,34]
+newNums=[]
+newNums.append(nums[0]*2)
+newNums.append(nums[1]*2)
+newNums.append(nums[2]*2)
+newNums.append(nums[3]*2)
+print(newNums)
