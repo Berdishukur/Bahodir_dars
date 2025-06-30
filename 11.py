750367b353ba68128101d9529dc4abcaa607c5ed
Berdishukur <boboqulovberdishukur@gmail.com>
2025-06-30 17:27:39 +0500

    add

--- 11.py
+++ 11.py
@@ -1,10 +1,3 @@
 # ism="Ali"
 # familiya="Valiyev"
 # yosh=22
-nums=[2,4,12,34]
-newNums=[]
-newNums.append(nums[0]*2)
-newNums.append(nums[1]*2)
-newNums.append(nums[2]*2)
-newNums.append(nums[3]*2)
-print(newNums)
